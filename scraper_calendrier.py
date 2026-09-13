cc3227d4db547517bfa97c82923f8601d2d9cdd3
fabz56120-ui <fabz56120@gmail.com>
2026-09-13 12:54:38 +0200

    Update scraper_calendrier.py

--- scraper_calendrier.py
+++ scraper_calendrier.py
@@ -1,4 +1,5 @@
 from playwright.sync_api import sync_playwright
+import json
 
 
 URL = "https://prod2.lnr.fr/calendrier-resultats"
@@ -26,10 +27,7 @@ def main():
 
         texte = page.locator("body").inner_text()
 
-        print(
-            f"{len(texte.splitlines())} lignes trouvées."
-        )
-
+        # Sauvegarde du debug
         with open(
             "debug_calendrier.txt",
             "w",
@@ -38,9 +36,27 @@ def main():
 
             fichier.write(texte)
 
-        print(
-            "Fichier debug_calendrier.txt créé."
-        )
+        # Pour l'instant, création du fichier JSON
+        donnees = {
+            "source": URL,
+            "calendrier": []
+        }
+
+        with open(
+            "calendrier.json",
+            "w",
+            encoding="utf-8"
+        ) as fichier:
+
+            json.dump(
+                donnees,
+                fichier,
+                ensure_ascii=False,
+                indent=4
+            )
+
+        print("debug_calendrier.txt créé.")
+        print("calendrier.json créé.")
 
         browser.close()
 
